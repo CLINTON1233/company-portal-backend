7754bce7263ba0333586d9873b2cfbd24a226b88
CLINTON1233 <PROJECT SIALAN 12>
2025-12-08 15:58:59 +0700

    company-portal

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Aktifkan CORS jika Next.js atau frontend lain ingin akses API
+  app.enableCors({
+    origin: '*',
+  });
+
+  // Set default port ke 6000 jika tidak ada PORT di .env
+  const port = process.env.PORT || 6000;
+  await app.listen(port);
+
+  console.log(` Server berjalan di http://localhost:${port}`);
 }
 bootstrap();
