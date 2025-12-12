1beec85022eaf8788368e79d89de0fd3e64b66b8
CLINTON1233 <PROJECT SIALAN 12>
2025-12-12 14:10:17 +0700

    http to https

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('ssl/localhost.key'),
+    cert: fs.readFileSync('ssl/localhost.crt'),
+  };
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    httpsOptions,
+  });
 
   app.enableCors({
     origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'http://localhost:3003',
+      'https://localhost:3000',
+      'https://localhost:3001',
+      'https://localhost:3002',
+      'https://localhost:3003',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // 🔥 expose folder uploads
+  // expose folder uploads
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
 
   await app.listen(4000);
-  console.log(`Server running on http://localhost:4000`);
+  console.log(`🚀 HTTPS Server running on https://localhost:4000`);
 }
+
 bootstrap();
